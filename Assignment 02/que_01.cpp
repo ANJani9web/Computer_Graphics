44797d9de38b73121edf4826f2ecab99599cde21
Anjani kumar <anjanison9@gmail.com>
2024-03-05 16:17:25 +0530

    Assignment 02 done

--- Assignment 02/que_01.cpp
+++ Assignment 02/que_01.cpp
@@ -1,63 +1,92 @@
 #include <GL/glut.h>
+#include<bits/stdc++.h> // Standard C++ library headers
+using namespace std;
 
-int x_coor = 200;
-int y_coor = 300;
+int x_coor, y_coor; // Variables to store the coordinates of the bottom-most left vertex of the rectangle
+int length, breadth; // Variables to store the length and breadth of the rectangle
+int x_mov, y_mov; // Variables to store the movement in x-direction and y-direction after clicking on the key
 
-void displayCB(void)
+// Display callback function
+void displayCB()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
 
     // Draw a colored polygon
+    glColor3f(0.4, 0.6, 0.4);  // Set color to a shade of green
     glBegin(GL_POLYGON);
-    glColor3f(0.6, 0.4, 0.2);  // color
-    glVertex2i(x_coor, y_coor);
-    glVertex2i(x_coor + 50, y_coor);
-    glVertex2i(x_coor + 30, y_coor + 150);
-    glVertex2i(x_coor + 80 , y_coor + 150);
+    glVertex2f(x_coor, y_coor);
+    glVertex2f(x_coor + breadth, y_coor);
+    glVertex2f(x_coor + breadth, y_coor + length);
+    glVertex2f(x_coor, y_coor + length);
     glEnd();
 
-    glFlush();
+    glFlush(); // Flush OpenGL buffers to display
 }
 
-void specialKeys(int key, int x, int y)
-{
-    switch (key)
-    {
-    case GLUT_KEY_UP:
-        y_coor += 10;
-        break;
-    case GLUT_KEY_DOWN:
-        y_coor -= 20;
-        break;
-    case GLUT_KEY_LEFT:
-        x_coor -= 30;
-        break;
-    case GLUT_KEY_RIGHT:
-        x_coor += 40;
-        break;
+// Function to handle special keyboard key events
+void Keys(int key, int x, int y)
+{   
+    // Move the rectangle based on the keys pressed
+    if(key == 101){ // Up arrow key
+        y_coor += y_mov;
+    }
+    if(key == 100){ // Left arrow key
+        x_coor -= x_mov;
+    }
+    if(key == 103){ // Down arrow key
+        y_coor -= y_mov;
+    }
+    if(key == 102){ // Right arrow key
+        x_coor += x_mov;
     }
 
-    if(x_coor<0) x_coor = 0;
-    if(x_coor>600) x_coor = 600;
-    if(y_coor<0) y_coor = 0;
-    if(y_coor>600) y_coor = 600;
+    // Ensure that the rectangle stays within the window boundaries
+    if(x_coor < 0) x_coor = 0;
+    if(x_coor + breadth > 600) x_coor = 600-breadth;
+    if(y_coor < 0) y_coor = 0;
+    if(y_coor + length > 600) y_coor = 600 - length;
 
     glutPostRedisplay();  // Request a redraw to update the display
 }
 
+// Main function
 int main(int argc, char *argv[])
-{
+{   
+    // Input the coordinates, length, breadth, and movement from the user
+    cout<<"Enter the coordinates of bottom-most left vertex of the Rectangle\n";
+    cout<<"X coor: ";
+    cin>>x_coor;
+    cout<<"Y coor: ";
+    cin>>y_coor;
+
+    cout<<"Enter the length and breadth of the rectangle"<<endl;
+    cout<<"Length: ";
+    cin>>length;
+    cout<<"Breadth: ";
+    cin>>breadth;
+    
+    cout<<"Enter the movement in x-direction and y-direction after clicking on the key\n";
+    cout<<"X-direction movement: ";
+    cin>>x_mov;
+    cout<<"Y-direction movement: ";
+    cin>>y_mov;
+
+    // Initialize GLUT
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
-    glutInitWindowSize(300, 300);
-    glutCreateWindow("Colored Polygon Shifting");
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Question_01 - Colored Polygon Shifting");
 
-    glClearColor(1, 1, 1, 0.0);
-    gluOrtho2D(0, 600, 0, 600);
+    // Set up the OpenGL environment
+    glClearColor(1, 1, 1, 0.0); // Set clear color to white
+    gluOrtho2D(0, 600, 0, 600); // Set up a 2D orthographic projection
+
+    // Register the display callback function and the special keys callback function
     glutDisplayFunc(displayCB);
-    glutSpecialFunc(specialKeys);  // Register the special keys callback
+    glutSpecialFunc(Keys);
 
+    // Enter the GLUT event processing loop
     glutMainLoop();
 
-    return 0;
+    return 0; // Return 0 to exit main function
 }
--- Assignment 02/que_02.cpp
+++ Assignment 02/que_02.cpp
@@ -1,75 +1,74 @@
 #include <GL/glut.h>
+#include<bits/stdc++.h> // Standard C++ library headers
+using namespace std;
 
-int x_coor = 300;
-int y_coor = 300;
-GLfloat color[3] = {1.0, 0.0, 0.0};  // Initial color: Red
+// Global variables to store the coordinates of the bottom-most left vertex of the rectangle
+int x_coor, y_coor;
 
+// Global variables to store the length and breadth of the rectangle
+int length, breadth;
+
+// Global variables to store the RGB components of the color
+float red = 1, blue = 0, green = 1;
+
+// Display callback function
 void displayCB(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
 
-    // Draw a colored polygon
+    // Draw a colored polygon with the specified color
+    glColor3f(red, green, blue);
     glBegin(GL_POLYGON);
-    glColor3fv(color);
-    glVertex2i(x_coor, y_coor);
-    glVertex2i(x_coor + 150, y_coor);
-    glVertex2i(x_coor + 150, y_coor + 150);
-    glVertex2i(x_coor, y_coor + 150);
+    glVertex2f(x_coor, y_coor);
+    glVertex2f(x_coor + breadth, y_coor);
+    glVertex2f(x_coor + breadth, y_coor + length);
+    glVertex2f(x_coor, y_coor + length);
     glEnd();
 
-    glFlush();
+    glFlush(); // Flush OpenGL buffers to display
 }
 
-void specialKeys(int key, int x, int y)
-{
-    switch (key)
-    {
-    case GLUT_KEY_UP:
-        y_coor += 10;
-        break;
-    case GLUT_KEY_DOWN:
-        y_coor -= 20;
-        break;
-    case GLUT_KEY_LEFT:
-        x_coor -= 30;
-        break;
-    case GLUT_KEY_RIGHT:
-        x_coor += 40;
-        break;
-    }
-
-    if(x_coor<0) x_coor = 0;
-    if(x_coor>600) x_coor = 600;
-    if(y_coor<0) y_coor = 0;
-    if(y_coor>600) y_coor = 600;
-
-    glutPostRedisplay();  // Request a redraw to update the display
-}
-
+// Mouse click callback function
 void mouseClick(int button, int state, int x, int y)
 {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)   // down means press
-    // left means left of the mouse
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
         // Change color to a random one on left-click
-        for (int i = 0; i < 3; ++i)
-            color[i] = (GLfloat)rand() / RAND_MAX;
-        glutPostRedisplay();  // Request a redraw to update the display
+        red = (GLfloat)rand() / RAND_MAX;
+        green = (GLfloat)rand() / RAND_MAX;
+        blue = (GLfloat)rand() / RAND_MAX;
+        glutPostRedisplay(); // Request a redraw to update the display
     }
 }
 
 int main(int argc, char *argv[])
 {
+    // Input the coordinates, length, breadth, and movement from the user
+    cout << "Enter the coordinates of bottom-most left vertex of the Rectangle\n";
+    cout << "X coor: ";
+    cin >> x_coor;
+    cout << "Y coor: ";
+    cin >> y_coor;
+
+    cout << "Enter the length and breadth of the rectangle" << endl;
+    cout << "Length: ";
+    cin >> length;
+    cout << "Breadth: ";
+    cin >> breadth;
+
+    // Initialize GLUT
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowSize(600, 600);
-    glutCreateWindow("Colored Polygon Shifting");
+    glutCreateWindow("Question_02 - Color Changing of the Polygon");
 
-    glClearColor(1, 1, 1, 0.0);
-    gluOrtho2D(0, 600, 0, 600);
+    // Set up the OpenGL environment
+    glClearColor(1, 1, 1, 0.0); // Set clear color to white
+    gluOrtho2D(0, 600, 0, 600); // Set up a 2D orthographic projection
+
+    // Register the display callback function and the mouse click callback function
     glutDisplayFunc(displayCB);
-    glutSpecialFunc(specialKeys);  // Register the special keys callback
-    glutMouseFunc(mouseClick);    // Register the mouse click callback
+    glutMouseFunc(mouseClick);
 
     glutMainLoop();
 
